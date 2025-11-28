fb552784f517fd88a9202226306b069588b8e515
Michal Conos <michal.conos@gmail.com>
2025-11-28 14:35:47 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# parquet-viewer
+
+## Overview
+
+Can view parquet files on object-store, including those in zip files. This makes
+easier navigation in real world datasets, e.g. from kaggle.
+
+## Usage
+```
+cd pblaha-parquet-testing.zip
+ls
+view parquet-testing/data/int32_decimal.parquet
+```
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use parquet_viewer::console;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut console = console::Console::new(
-        // "https://objectstorage.us-phoenix-1.oraclecloud.com/n/oraclebigdatadb/b/bucket-20250414-1013/o/",
-        "/mnt/c/Users/micha/Downloads/",
+        "https://objectstorage.us-phoenix-1.oraclecloud.com/n/oraclebigdatadb/b/bucket-20250414-1013/o/",
+        // "/mnt/c/Users/micha/Downloads/",
     )?;
     console.process_console_input().await;
     Ok(())
